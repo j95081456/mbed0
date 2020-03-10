dc29ca028eabf9055bb98577885ff4fd877838e3
j95081456 <j95081456@gmail.com>
2020-03-11 00:03:12 +0800

    factorial

--- test/main.cpp
+++ test/main.cpp
@@ -1,36 +1,36 @@
-#include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-void print(int num);
+int factorial(int n);
 
 int main(){
     int n;
+    int k;
     cin >> n;
     int a[n];
-    for (int i=0 ; i<n; i++){
+    for (int i=0 ; i<n ; i++){
         cin >> a[i];
     }
     for (int i=0 ; i<n ; i++){
-        print(a[i]);
-    }
-}
-
-void print(int num){
-    int a[num][num];
-    for(int i=0; i<num; i++){
-        for (int j=0; j<=i; j++){
-            if(j==0 || j==i){
-               a[i][j]=1;
-               if (j==0) cout << 1;
-               else cout << " " << 1;
-            }
-            else {
-                a[i][j]=a[i-1][j-1] + a[i-1][j];
-                cout << " " <<a[i][j];
+        for (int j=0 ; j<a[i]; j++){
+            for (int p=0 ; p<=j ; p++){
+            if (j==0 && p==0){ cout << 1;}
+            else if (j == p && j!=0){ cout << 1;}
+            else {  
+                k = factorial(j)/(factorial(j-p)*factorial(p));
+                    cout << k << " ";
             }
         }
         cout << endl;
+      }
     }
+
+
+}
+
+int factorial(int n){
+    if (n==1 || n==0) return 1;
+    else return factorial(n-1)*n;
+    
 }
