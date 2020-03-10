4bf67da163732108d8701050d4afbe6278cbc05b
j95081456 <j95081456@gmail.com>
2020-03-10 23:05:20 +0800

    3/10

--- test/main.cpp
+++ test/main.cpp
@@ -3,34 +3,34 @@
 
 using namespace std;
 
-int search(int *a , int n , int target);
+void print(int num);
 
-int main(void){
+int main(){
     int n;
     cin >> n;
     int a[n];
-    for (int i=0; i<n; i++){
-    cin >> a[i];
+    for (int i=0 ; i<n; i++){
+        cin >> a[i];
     }
-    int t;
-    cin >> t;
-    int i = search(a,n,t);
-    cout << a[i];
-    return 0;
+    for (int i=0 ; i<n ; i++){
+        print(a[i]);
+    }
 }
 
-int search(int *a , int n , int target){
-     int middle;
-     int left = 0;
-     int right = n-1;
-   while (left<=right){
-       middle = (left + right)/2;
-       if(target < a[middle]){
-           right = middle - 1;
-       }else if (target > a[middle]){
-           left = middle + 1;
-       }
-       else return middle;
-   }
-   return -1 ;
+void print(int num){
+    int a[num][num];
+    for(int i=0; i<num; i++){
+        for (int j=0; j<=i; j++){
+            if(j==0 || j==i){
+               a[i][j]=1;
+               if (j==0) cout << 1;
+               else cout << " " << 1;
+            }
+            else {
+                a[i][j]=a[i-1][j-1] + a[i-1][j];
+                cout << " " <<a[i][j];
+            }
+        }
+        cout << endl;
+    }
 }
