b71929868587dcbe9f364d440cfefb68a4c99c44
j95081456 <j95081456@gmail.com>
2020-03-17 20:51:10 +0800

    HW1

---
+++ test/123.py
@@ -0,0 +1,133 @@
+
+# Part. 1
+
+#=======================================
+
+# Import module
+
+#  csv -- fileIO operation
+
+import csv
+
+#=======================================
+
+
+# Part. 2
+
+#=======================================
+
+# Read cwb weather data
+
+cwb_filename = 'sample.csv'
+
+data = []
+
+header = []
+
+with open(cwb_filename) as csvfile:
+
+   mycsv = csv.DictReader(csvfile)
+
+   header = mycsv.fieldnames
+
+   for row in mycsv:
+      data.append(row)    # put row into data
+   
+#=======================================
+
+
+# Part. 3
+
+#=======================================
+
+# Analyze data depend on your group and store it to target_data like:
+
+# Retrive all data points which station id is "C0X260" as a list.
+
+target_data = list(filter(lambda item: (
+                                        item['station_id'] == 'C0A880' or item['station_id'] == 'C0F9A0' or item['station_id'] == 'C0G640' or item['station_id'] == 'C0R190' or item['station_id'] == 'C0X260'
+                                        )
+                                        , data))
+
+# Retrive ten data points from the beginning.
+
+# target_data = data[:10]
+
+
+#=======================================
+
+
+# Part. 4
+
+#=======================================
+
+# Print result
+
+
+HUMD_C0A880= 0.0
+HUMD_C0F9A0 = 0.0
+HUMD_C0G640 = 0.0
+HUMD_C0R190 = 0.0
+HUMD_C0X260 = 0.0
+
+for i in range(len(target_data)):
+   if (target_data[i]['station_id'] == 'C0A880') :
+      if (target_data[i]['HUMD'] == '-99.000' or
+            target_data[i]['HUMD'] == '-999.000') :
+            HUMD_C0A880 = HUMD_C0A880
+      else :
+         HUMD_C0A880 = HUMD_C0A880 + float(target_data[i]['HUMD'])
+   elif (target_data[i]['station_id'] == 'C0F9A0') :
+      if (target_data[i]['HUMD'] == '-99.000' or 
+            target_data[i]['HUMD'] == '-999.000') :
+            HUMD_C0F9A0 = HUMD_C0F9A0
+      else :
+         HUMD_C0F9A0 = HUMD_C0F9A0 + float(target_data[i]['HUMD'])
+   elif (target_data[i]['station_id'] == 'C0G640') :
+      if (target_data[i]['HUMD'] == '-99.000' 
+            or target_data[i]['HUMD'] == '-999.000') :
+            HUMD_C0G640 = HUMD_C0G640
+      else :
+         HUMD_C0G640 = HUMD_C0G640 + float(target_data[i]['HUMD'])
+   elif (target_data[i]['station_id'] == 'C0R190') :
+      if (target_data[i]['HUMD'] == '-99.000' or 
+            target_data[i]['HUMD'] == '-999.000') :
+            HUMD_C0R190 = HUMD_C0R190
+      else :
+         HUMD_C0R190 = HUMD_C0R190 + float(target_data[i]['HUMD'])
+   elif (target_data[i]['station_id'] == 'C0X260') :
+      if (target_data[i]['HUMD'] == '-99.000' or 
+            target_data[i]['HUMD'] == '-999.000') :
+            HUMD_C0X260 = HUMD_C0X260
+      else :
+         HUMD_C0X260 = HUMD_C0X260 + float(target_data[i]['HUMD'])
+
+if (HUMD_C0A880 == 0) :
+   VALUE_C0A880 = str(None)
+else :
+   VALUE_C0A880 = str(format(HUMD_C0A880, '.2f'))
+if (HUMD_C0F9A0 == 0) :
+   VALUE_C0F9A0 = str(None)
+else :
+   VALUE_C0F9A0 = str(format(HUMD_C0F9A0, '.2f'))
+if (HUMD_C0G640 == 0) :
+   VALUE_C0G640 = str(None)
+else :
+   VALUE_C0G640 = str(format(HUMD_C0G640, '.2f'))
+if (HUMD_C0R190 == 0) :
+   VALUE_C0R190 = str(None)
+else :
+   VALUE_C0R190 = str(format(HUMD_C0R190, '.2f'))
+if (HUMD_C0X260 == 0) :
+   VALUE_C0X260 = str(None)
+else :
+   VALUE_C0X260 = str(format(HUMD_C0X260, '.2f'))
+
+
+print('''[['C0A880',''', VALUE_C0A880 + '],',
+         '''['C0F9A0',''', VALUE_C0F9A0 + '],', 
+            '''['C0G640',''', VALUE_C0G640 + '],', 
+                '''['C0R190',''', VALUE_C0R190 + '],', 
+                    '''['C0X260',''', VALUE_C0X260 + ']]')   
+
+#========================================
